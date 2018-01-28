870501aea631470514f34ae2ef3b7b373ef71394
NICooper <nicholas.i.cooper@gmail.com>
2018-01-27 18:28:10 -0800

    WaveWIP

--- GGJ2018_Transmission/Assets/Scripts/Waves/Wave.cs
+++ GGJ2018_Transmission/Assets/Scripts/Waves/Wave.cs
@@ -2,15 +2,53 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Wave : MonoBehaviour {
+public class Wave : Entity
+{
+    public Wavelet waveletPrefab;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public float wavePower = 10.0f;
+    public List<Wavelet> wavelets;
+    public int angle = 30;
+    public int numWavelets = 5;
+    public float distToWaveletSpawn = 0.5f;
+
+    void Start()
+    {
+        CreateWavelets();
+    }
+
+    private void CreateWavelets()
+    {
+        for (int i = 0; i < numWavelets; i++)
+        {
+            var w = Instantiate(waveletPrefab, transform);
+            w.transform.parent = transform;
+            wavelets.Add(w);
+        }
+    }
+
+    void Scan()
+    {
+
+    }
+
+    public void UpdateWaveAngle(int newAngle)
+    {
+        angle = newAngle;
+
+        // add subtract wavelets and sync their phases
+    }
+
+
+    public void Disengage()
+    {
+        // For testing. Later this should just prepare the wave to collapse.
+        for (int i = 0; i < wavelets.Count; i++)
+        {
+            var w = wavelets[i];
+            wavelets.Remove(w);
+            Destroy(w);
+        }
+        Destroy(this);
+    }
 }
